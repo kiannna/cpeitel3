57a845b207c6392116638dac3dfbfe050040754e
iaaees <143621144+iaaees@users.noreply.github.com>
2023-09-14 11:03:35 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm Kianna Reyes, 19 years old, from Sta. Maria, Mexico, Pampanga. Dancing is my hobby. When it
-comes to music, my preferences vary depending on my mood, although I often choose k-pop songs whenever I
-feel like dancing. Sinigang and adobo are my favorite foods. Despite my love for dancing, I sometimes feels self-
-conscious about my height, as I stand at only 4’11 or 5’. Alongside my interest in dance, I am a fan of BTS, I also
-enjoy watching movies, although I tend to sleep halfway through most of the time.</p>
+        <p>10 Things That Require Zero Talent</p>
+
+        <p>1. Being On Time </p>
+        <p>2. Making An Effort </p>
+        <p>3. Being High Energy</p>
+        <p>4. Having A Positive Attitude</p>
+        <p>5. Being Passionate</p>
+        <p>6. Using Good Body Language</p>
+        <p>7. Being Coachable</p>
+        <p>8. Doing A Little Extra</p>
+        <p>9. Being Prepared</p>
+        <p>10. Having A Strong Work Ethic</p>
+  
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
